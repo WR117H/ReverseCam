fdff82a13fae21d5c13ba330d38c3f73ac5a4085
Wraith <97615989+WR117H@users.noreply.github.com>
2023-06-16 01:28:57 +0330

    Update client.py

--- client.py
+++ client.py
@@ -2,37 +2,61 @@ import cv2
 import socket
 import pickle
 import struct
+import argparse
 
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Server')
+parser.add_argument('--ip', type=str, help='Server IP address')
+parser.add_argument('--port', type=int, help='Server port number')
+args = parser.parse_args()
 
 # Create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '0.0.0.0'  # Replace with the server IP address
-port = 9999
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
-client_socket.connect((host_ip, port))
+server_socket.bind((args.ip, args.port))
 
-# Open the webcam
-cap = cv2.VideoCapture(0)
+# Listen for incoming connections
+server_socket.listen(5)
+
+print("Waiting for a client to connect...")
+
+# Accept a client connection
+client_socket, addr = server_socket.accept()
+print('Connected to:', addr)
+
+# Initialize a variable to store the received frame
+data = b""
+payload_size = struct.calcsize("Q")
 
 while True:
-    # Read a frame from the webcam
-    ret, frame = cap.read()
+    # Receive data from the client
+    while len(data) < payload_size:
+        packet = client_socket.recv(4 * 1024)  # Adjust the buffer size as needed
+        if not packet:
+            break
+        data += packet
 
-    # Serialize the frame using pickle
-    data = pickle.dumps(frame)
+    # Extract the length of the serialized frame from the received data
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-    # Pack the serialized frame and its length into a struct
-    message = struct.pack("Q", len(data)) + data
+    # Keep receiving data until the entire frame is received
+    while len(data) < msg_size:
+        data += client_socket.recv(4 * 1024)  # Adjust the buffer size as needed
 
-    # Send the frame to the server
-    client_socket.sendall(message)
+    # Deserialize the received frame using pickle
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
 
-    # Break the loop if 'Enter' is pressed
-    if cv2.waitKey(1) == 13:
+    # Display the received frame
+    cv2.imshow('Server - Receiving Webcam', frame)
+    if cv2.waitKey(1) == 13:  # Press 'Enter' to exit
         break
 
 # Release resources
-cap.release()
 cv2.destroyAllWindows()
 client_socket.close()
+server_socket.close()
